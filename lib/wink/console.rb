76587be38437803961436ea2cbd0377c582ff631
Jimmy Kittiyachavalit <jimmy@codetry.com>
2008-07-24 10:22:47 +0700

    Console now responds to reset! and reload!.

---
+++ lib/wink/console.rb
@@ -0,0 +1,7 @@
+def reload!
+  Wink::Models.reload!
+end
+
+def reset!
+  Wink::Schema.reset!
+end
--- lib/wink/models.rb
+++ lib/wink/models.rb
@@ -1,2 +1,14 @@
 require 'wink/models/article'
 require 'wink/schema'
+
+module Wink
+  module Models
+    extend self
+
+    def reload!
+      Object.send(:remove_const, :Article)
+      DataMapper::Resource.descendants.clear
+      load 'lib/wink/models/article.rb'
+    end
+  end
+end
